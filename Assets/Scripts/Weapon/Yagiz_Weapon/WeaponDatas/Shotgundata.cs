b046879c5f0b1b77238c021bb57b35387cde38b9
Yağız <ygzkrt@gmail.com>
2024-03-09 00:47:13 +0300

    weapon continue

--- Assets/Scripts/Weapon/Yagiz_Weapon/WeaponDatas/Shotgundata.cs
+++ Assets/Scripts/Weapon/Yagiz_Weapon/WeaponDatas/Shotgundata.cs
@@ -5,15 +5,4 @@ using UnityEngine;
 public class Shotgundata : RangedWeaponData
 {
     public float spread;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
